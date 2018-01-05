8d7f4e22aee8b0f379996bf6a0faa28acf302eaa
YounGoat <youngoat@163.com>
2018-01-06 00:45:43 +0800

    .

--- bin/portman.js
+++ bin/portman.js
@@ -1,1 +1,27 @@
 #!/usr/bin/env node
+
+'use strict';
+
+const MODULE_REQUIRE = 1
+    /* built-in */
+
+    /* NPM */
+    , noda = require('noda')
+    
+    /* in-package */
+    , seekUsable = noda.inRequire('lib/seekUsable')
+    ;
+
+const argv = process.argv.slice(2);
+
+if (argv.length && [ 'seekusable' ].includes(argv[0].toLowerCase())) {
+    let rangeCode = argv[1];
+    seekUsable(rangeCode).then((port) => {
+        console.log(`The minimal usable port is ${port}.`);
+    }).catch((err) => {
+        console.err(err instanceof Error ? err.message : err);
+    })
+}
+else {
+    console.log(noda.inRead('help.txt', 'utf8'));
+}
